eddd385d15e6accee8546022158dd833e30a4e22
Bollineni satish chandra kumar <sbolline2@gitam.in>
2024-10-09 09:02:13 +0530

    Update hello.py

--- hello.py
+++ hello.py
@@ -2,4 +2,4 @@ name = "satish"
 age = "19"
 studying = "final year"
 university = "GITAM"
-print(f"my name is {name} and i am {age} old {studying} at {university}
+print(f"my name is {name} and i am {age} old {studying} at {university}")
